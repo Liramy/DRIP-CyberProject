f6258044420663dcd2288ffc687eaf0ec442b6f0
Liramy <lirammash1@gmail.com>
2024-02-23 14:29:30 +0200

    Add: better documantation for model. Fix: article scraping, propaganda precentege calculations

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,9 @@
 import joblib
+import sklearn
 import GoogleNews
 from newspaper import Article
 import base64
+from urllib.parse import urlparse, parse_qs, unquote
 
 class ArticleFinder():
     """Define an object of type <ArticleFinder> for getting articles off the internet
@@ -18,6 +20,9 @@ class ArticleFinder():
         self.language = configs['lang']
         self.period = configs['period']
 
+        self.model = joblib.load("PropDetectionModel.mdl")
+        self.count_vectorize = joblib.load("CountVectorizer.vct")
+
         self.google_search = GoogleNews.GoogleNews(lang=self.language)
         self.google_search.enableException(True)
         self.google_search.setperiod(self.period)
@@ -26,19 +31,59 @@ class ArticleFinder():
 
         max_articles_number = 25
         self.articleLinkList = self.google_search.get_links()[:max_articles_number]
+        for i in range(len(self.articleLinkList)):
+            self.articleLinkList[i] = self.decipher_url(self.articleLinkList[i])
+            
+        self.create_propaganda_isolation()
 
     def close(self):
         self.google_search.clear()
 
+    def decipher_url(self, encrypted_url):
+        parsed_url = urlparse(encrypted_url)
+        encoded_path = parsed_url.path.split("/")[-1]
+        decoded_path = unquote(encoded_path)
 
-dicty = {"subject": "racism", "lang": "en", "period": "4y"}
+        query_params = parse_qs(parsed_url.query)
+
+        if 'hl' in query_params:
+            language = query_params['hl'][0]
+        else:
+            language = 'en'  # Default to English if 'hl' parameter is not present
+
+        return f'https://news.google.com/articles/{decoded_path}?hl={language}'
+
+    def create_propaganda_isolation(self):
+        article_array = []
+        for article_url in self.articleLinkList:
+            try:
+                article = Article(article_url)
+                article.download()
+                article.parse()
+                exclude_chars = ["", ".", "\n"]
+
+                # Split the text into an array of words, excluding specified characters
+                text = [sentence.strip() for sentence in article.text.split('.') if
+                        sentence.strip() not in exclude_chars]
+                article_array.append(text)
+            except Exception as e:
+                pass
+        i = 0
+        self.predsArticles = []
+        for article_text in article_array:
+            if len(article_text) < 3:
+                pass
+            x_value = self.count_vectorize.transform(article_text)
+            predictions = self.model.predict(x_value)
+            count_of_ones = sum(1 for value in predictions if value == 1)
+            average_prediction = (count_of_ones / len(predictions))
+            self.predsArticles.append((article_text, average_prediction))
+
+    def printPreds(self):
+        for pred in self.predsArticles:
+            print(pred[1])
+
+
+dicty = {"subject": "racism", "lang": "en", "period": "3y"}
 finderz = ArticleFinder(dicty)
-
-print(finderz.articleLinkList)
-
-google_url = "https://news.google.com/articles/CBMiZmh0dHBzOi8vd3d3LnB1c2hzcXVhcmUuY29tL2d1aWRlcy9ob3Jpem9uLWZvcmJpZGRlbi13ZXN0LWJ1cm5pbmctc2hvcmVzLWFsbC1kZWx2ZXJzLXRyaW5rZXRzLWxvY2F0aW9uc9IBAA?hl=en-CA&gl=CA&ceid=CA%3Aen"
-base64_url = google_url.replace("https://news.google.com/articles/","").split("?")[0]
-actual_url = base64.b64decode(base64_url)[4:-3].decode('utf-8')
-print(actual_url)
-
-#TODO: Turn news.google.com/articles into normal url for the newspaper3k to process
+print(finderz.printPreds())
