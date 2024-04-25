5eca0a362ae39221776e8878bd33797641382392
Liramy <lirammash1@gmail.com>
2024-04-25 16:23:24 +0300

    Add: Article scrapping that makes a dictionary of links, text, predictions.

--- Server/ArticleScrapping.py
+++ Server/ArticleScrapping.py
@@ -6,17 +6,19 @@ import base64
 from urllib.parse import urlparse, parse_qs, unquote
 
 
-class ArticleScrapper():
+class ArticleScrapper:
     """Define an object of type <ArticleFinder> for getting articles off the internet
 
-    :param configs: Contains the list of configs for the search results
-    :type configs: dict
+    :param subject: Subject of the searching function - String
+    :param period: The period of time for the search (i.e. 7d, 2m, 5y) - String
+    :param max_results: max number of results for search - int
+    :param language: Language with the abbreviation (En, Es ...) - String
 
     :returns: An object for getting articles of the internet
     :rtype: ArticleScrapper
     """
 
-    def __init__(self, subject, period, language='En'):
+    def __init__(self, subject, period, max_results=25, language='En'):
         self.subject = subject
         self.language = language
         self.period = period
@@ -30,17 +32,23 @@ class ArticleScrapper():
         self.google_search.set_encode('utf-8')
         self.google_search.get_news(self.subject)
 
-        max_articles_number = 25
-        self.articleLinkList = self.google_search.get_links()[:max_articles_number]
+        max_results = len(self.google_search.get_links()) if (
+                len(self.google_search.get_links()) < max_results) else max_results
+        self.articleLinkList = self.google_search.get_links()[:max_results]
         for i in range(len(self.articleLinkList)):
             self.articleLinkList[i] = self.decipher_url(self.articleLinkList[i])
 
         self.create_propaganda_isolation()
 
     def close(self):
+        """Close the scrapper to not waste cpu usage"""
         self.google_search.clear()
 
     def decipher_url(self, encrypted_url):
+        """The url given is a Google News link, those links are inherently encrypted.
+           The encryption used by Google is a basic base64 cypher which is easy to decode.
+           We simply decypher the path, then the language and we get the finished link.
+        """
         parsed_url = urlparse(encrypted_url)
         encoded_path = parsed_url.path.split("/")[-1]
         decoded_path = unquote(encoded_path)
@@ -51,11 +59,12 @@ class ArticleScrapper():
             language = query_params['hl'][0]
         else:
             language = 'en'  # Default to English if 'hl' parameter is not present
-
         return f'https://news.google.com/articles/{decoded_path}?hl={language}'
 
     def create_propaganda_isolation(self):
-        self.article_array = []
+        """Make an array of every article and the propaganda precentege within it"""
+        article_array = []
+        article_dict = {}
         for article_url in self.articleLinkList:
             try:
                 article = Article(article_url)
@@ -66,23 +75,25 @@ class ArticleScrapper():
                 # Split the text into an array of words, excluding specified characters
                 text = [sentence.strip() for sentence in article.text.split('.') if
                         sentence.strip() not in exclude_chars]
-                self.article_array.append(text)
+                article_array.append(text)
+                article_dict[text[0]] = article_url
             except Exception as e:
                 pass
-        i = 0
-        self.predsArticles = []
-        for article_text in self.article_array:
+        self.refactored_dict = {}
+        for article_text in article_array:
+            text = article_text
             if len(article_text) < 3:
                 pass
-            x_value = self.count_vectorize.transform(article_text)
-            predictions = self.model.predict(x_value)
-            count_of_ones = sum(1 for value in predictions if value == 1)
-            average_prediction = (count_of_ones / len(predictions))
-            self.predsArticles.append((article_text, average_prediction))
+            try:
+                x_value = self.count_vectorize.transform(article_text)
+                predictions = self.model.predict(x_value)
+                count_of_ones = sum(1 for value in predictions if value == 1)
+                average_prediction = (count_of_ones / len(predictions))
+                print(average_prediction)
+                url = article_dict[text[0]]
+                self.refactored_dict[url] = (text, average_prediction)
+            except Exception as e:
+                pass
 
-    def return_preds(self):
-        return self.predsArticles
-
-    def return_texts(self):
-        return self.article_array
-
+    def return_dict(self):
+        return self.refactored_dict
