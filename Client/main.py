6cb5c9fe48bd5c174b2efecce48ba4d31e4e2bab
Liramy <lirammash1@gmail.com>
2024-04-27 21:22:00 +0300

    Add: `main.py` file for the main client code and sockets

---
+++ Client/main.py
@@ -0,0 +1,7 @@
+import socket
+
+client_socket = socket.socket
+Server_IP = "0.0.0.0" # Placeholder until we have an actual ip adress
+Server_PORT = 8202
+
+# TODO: Create the login app here and a function for giving and relying data
