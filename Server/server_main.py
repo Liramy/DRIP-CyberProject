64038dd0f1b3098f875e407063f11dad2a23cc5d
Liramy <lirammash1@gmail.com>
2024-04-29 19:04:29 +0300

    Add: Code for main server. TODO: add client code and handeling for events.

---
+++ Server/server_main.py
@@ -0,0 +1,24 @@
+import socket
+import threading
+
+def handle_clients(client, addr):
+    print(f'client address: {address}')
+
+host = '0.0.0.0'
+port = 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen(5)
+
+print(socket.gethostbyname(socket.gethostname()))
+
+threads = []
+
+while True:
+    (client_socket, address) = server.accept()
+    
+    t = threading.Thread(target=handle_clients, args=(client_socket, address))
+    t.start()
+    
+    threads.append(t)
+    print("threading.active_count()")
